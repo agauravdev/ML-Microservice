1e8f79929b88a4f312678456d85ee5f58d5a2296
EC2 Default User <ec2-user@ip-172-31-22-13.us-west-2.compute.internal>
2020-04-14 15:42:22 +0000

    ..

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,7 @@
 !/usr/bin/env bash
 
-docker build . -t udacity_devops_4
+docker build . --tag=udacity_devops_4
 
 docker image ls
 
-docker run -p 8000:80 udacity_devops_4
+docker run -p 8085:80 udacity_devops_4
